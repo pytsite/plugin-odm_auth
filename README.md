b0460e1ae866888f58af45fa53c2384773e1ca4f
Oleksandr Shepetko <a@shepetko.com>
2018-10-11 04:10:23 +0300

    Release 2.0

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 ## Changelog
 
 
+### 2.0 (2018-10-11)
+
+Suport of `pytsite-8.x`.
+
+
 ### 1.9.1 (2018-09-14)
 
 Unnecessary permissions checking removed.
--- __init__.py
+++ __init__.py
@@ -7,9 +7,3 @@ __license__ = 'MIT'
 # Public API
 from . import _model as model
 from ._api import check_permission
-
-
-def plugin_load():
-    from pytsite import lang
-
-    lang.register_package(__name__)
